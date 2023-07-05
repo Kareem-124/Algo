13092bb31be2b7e7a42e2c679b63402e613536b5
Kareem Malhis <kareem-124@hotmail.com>
2023-07-05 23:08:10 +0300

    upload completed

--- string_to_do_1.js
+++ string_to_do_1.js
@@ -29,6 +29,10 @@ function keep_numbers(string) {
     string = remove_blanks(string);
     // iterate through the filtered string.
     for (let i = 0; i < string.length; i++) {
+        // if the first number is 0 neglect it.
+        if(string[i] == 0 && new_string == ""){
+            continue;
+        }
         // if the character is not NaN (Not a Number) "The character is a number"
         if (!(isNaN(Number(string[i])))) {
             new_string = new_string + string[i];
@@ -37,5 +41,47 @@ function keep_numbers(string) {
     return new_string
 }
 
-str2="123 Test 456"
+str2="000s1a3y5w7h9a2t4?6!8?0"
 console.log(keep_numbers(str2));
+
+//---------------------------------------------------------------------
+//Acronyms
+function acronym(string){
+    let new_string = "";
+    string = string.split(' ');
+    console.log(string);
+    for (let i = 0 ; i < string.length ; i++){
+        if (string[i] != ''){
+            new_string = new_string + string[i][0].toUpperCase();
+        }
+    }
+    return new_string
+}
+
+console.log(acronym(" there's no free lunch - gotta pay yer way. "));
+console.log(acronym("Live from New York, it's Saturday Night!"));
+
+//---------------------------------------------------------------------
+//Count Non-Spaces
+function countNonSpaces(string){
+    return remove_blanks(string).length;
+}
+console.log(countNonSpaces("Honey pie, you are driving me crazy"));
+console.log(countNonSpaces("Hello world !"));
+
+//---------------------------------------------------------------------
+//Remove Shorter Strings
+function removeShorterStrings(array, min_val){
+    let counter = 0;
+    let new_array = [];
+    for (let i = 0; i <array.length; i++){
+        if(array[i].length >= min_val){
+            new_array[counter] = array[i];
+            counter++;
+            // I can use Push instead but built-in function is not allowed here
+        }
+    }
+    return new_array
+}
+console.log(removeShorterStrings(['Good morning', 'sunshine', 'the', 'Earth', 'says', 'hello'], 4));
+console.log(removeShorterStrings(['There', 'is', 'a', 'bug', 'in', 'the', 'system'],3));
